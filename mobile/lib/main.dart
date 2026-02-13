1880bffe42b403b9ecf5f2edd679c5889931d671
Mboya Michael <mikemboya2@gmail.com>
2026-02-13 13:53:58 +0300

    Watch auth provider in myapp

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile/providers/auth/auth_notifier_provider.dart';
 import 'package:mobile/providers/supabase/supabase_client_provider.dart';
 import 'package:mobile/router/app_router.dart';
 import 'package:mobile/ui/theme/theme.dart';
@@ -19,6 +20,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ref.watch(supabaseClientProvider);
+    ref.watch(authProvider);
 
     return MaterialApp.router(
       routerConfig: _appRouter.config(),
